81d7de8b85804e64773f54a8ad83cfde7675a699
Mark walsh <Mark.walsh@ddls.com.au>
2022-05-31 13:19:39 +1000

    init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DDLSWebApp.Models;
+using Microsoft.Extensions.Configuration;
+
+
+
+namespace DDLSWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IConfiguration Configuration;
+ 
+        public HomeController(IConfiguration _configuration)
+        {
+            Configuration = _configuration;
+        }
+
+        public IActionResult Index()
+        {
+            int valueToCube=0;
+            int.TryParse(Request.Query["number"],out valueToCube);
+            ViewBag.Number=valueToCube;
+            //ViewBag.Result=MathService.Class1.Cube(valueToCube);
+            ViewBag.Result="**Placeholder**";
+            ViewBag.Colour = Configuration["Colour"];
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Introduction 
+This is the WEb UI
+
+
+[![Build Status](https://dev.azure.com/mwjun221/DDLSWebApp/_apis/build/status/DDLSWebApp?branchName=main)](https://dev.azure.com/mwjun221/DDLSWebApp/_build/latest?definitionId=6&branchName=main)
